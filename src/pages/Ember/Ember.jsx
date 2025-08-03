5840d79c003de6d26ae2d1b7e4aa52e5fbbeb43c
Roshan Shibu <roshanshibu97@gmail.com>
2025-08-03 13:48:35 +0200

    implement player UI structure

--- src/pages/Ember/Ember.jsx
+++ src/pages/Ember/Ember.jsx
@@ -1,13 +1,17 @@
 import "./Ember.css";
 import Player from "../../views/Player/Player";
+import { useState } from "react";
 
 export default function Ember({ setLoggedIn, serverURL, accessToken }) {
+  const [isSmallPlayer, setIsSmallPlayer] = useState(true);
   return (
     <main className="emberMainContainer">
       <Player
         {...{
           serverURL,
           accessToken,
+          isSmallPlayer,
+          setIsSmallPlayer,
         }}
       />
       <p>Ember Main App</p>
--- src/views/Player/Player.jsx
+++ src/views/Player/Player.jsx
@@ -2,7 +2,12 @@ import { useEffect, useRef } from "react";
 import "./Player.css";
 import Hls from "hls.js";
 
-export default function Player({ serverURL, accessToken }) {
+export default function Player({
+  serverURL,
+  accessToken,
+  isSmallPlayer,
+  setIsSmallPlayer,
+}) {
   const audioRef = useRef();
 
   useEffect(() => {
@@ -30,15 +35,70 @@ export default function Player({ serverURL, accessToken }) {
     }
   }, []);
   return (
-    <article className="PlayerContainer">
-      <p>Player here</p>
+    <article
+      className={`PlayerContainer ${isSmallPlayer && "smallPlayer"}`}
+      onClick={() => {
+        if (isSmallPlayer) {
+          setIsSmallPlayer(false);
+        }
+      }}
+    >
       <audio
         preload="true"
         id="AudioPlayer"
         ref={audioRef}
         controls
         crossOrigin="anonymous"
+        style={{ display: "none" }}
       ></audio>
+      <div className="mobileWidthControl">
+        <div className="topControlsContainer">
+          <div
+            className="controlsContainer"
+            onClick={() => {
+              setIsSmallPlayer(true);
+            }}
+          >
+            <img src="Arrow.svg" />
+            Back
+          </div>
+          <div className="controlsContainer">
+            <img src="Queue.svg" />
+            Queue
+          </div>
+        </div>
+        <div className="albumArt"></div>
+        <p className="songName">Song Name Here Song Name Here Song Name Here</p>
+
+        <section>
+          <div className="artistAlbumContainer">
+            <p>Artist Name Here</p>
+            <p>Album Name Here</p>
+          </div>
+          <input
+            type="range"
+            min="0"
+            max="178.654671"
+            className="timeline"
+          ></input>
+          <div className="elapsedTimeContainer">
+            <p>0:11</p>
+            <p>3:12</p>
+          </div>
+        </section>
+
+        <div className="playbackControlsContainer">
+          <div>
+            <img src="Previous.svg" />
+          </div>
+          <div>
+            <img src="Play.svg" />
+          </div>
+          <div>
+            <img src="Next.svg" />
+          </div>
+        </div>
+      </div>
     </article>
   );
 }
