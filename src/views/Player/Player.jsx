cb78a4a1e0840e6a28c3108bb97dc8ad590bd1ea
Roshan Shibu <roshanshibu97@gmail.com>
2025-11-01 23:36:54 +0100

    remove eager play behavior

--- src/views/Player/Player.jsx
+++ src/views/Player/Player.jsx
@@ -51,7 +51,6 @@ export default function Player({
         console.error(err);
       });
     }
-    playMusic();
 
     const albumArtSrc = `${serverURL}/Music/${uuid}/${uuid}.png`;
     const albumArtOptions = {
